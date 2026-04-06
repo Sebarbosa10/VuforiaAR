a138f3d547330a355409e78b62348dd992f27596
Sebarbosa10 <sebastianbarbosa10@gmail.com>
2026-04-06 01:30:15 -0300

    vuforia esta ready

---
+++ VuforiaAR/Assets/Resources/AbrirLink.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class AbrirLink : MonoBehaviour
+{
+    public void AbrirGitHub()
+    {
+        Application.OpenURL("https://github.com/Sebarbosa10/VRGame.git");
+    }
+}
